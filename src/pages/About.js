fd80b3ba72c711dd7d541b93a593ca7d13041594
thobenayann <thobena.yann@orange.fr>
2023-08-20 09:03:11 +0200

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,42 @@
 import React from 'react';
+// images
+import WomanImg from '../img/about/woman.png';
+// link
+import { Link } from 'react-router-dom';
 
 const About = () => {
-    return <section className='section bg-yellow-200'>About</section>;
+    return (
+        <section className='section'>
+            <div className='container mx-auto h-full relative'>
+                {/* text & img wrapper */}
+                <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+                    {/* image */}
+                    <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+                        <img src={WomanImg} alt='woman image' />
+                    </div>
+                    {/* text */}
+                    <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+                        <h1 className='h1'>About me</h1>
+                        <p className='mb-12 max-w-sm'>
+                            Sunt amet id exercitation anim. Velit exercitation
+                            <b> incididunt consectetur</b> ad id esse velit
+                            ipsum elit nostrud labore id magna et. Id duis nisi
+                            amet labore occaecat do aliquip quis mollit.
+                            <br />
+                            <br />
+                            Sunt amet id exercitation anim. Velit exercitation
+                            incididunt consectetur ad id esse velit ipsum elit
+                            nostrud labore id magna et. Id duis nisi amet labore
+                            occaecat do aliquip quis mollit.
+                        </p>
+                        <Link to={'/portfolio'} className='btn'>
+                            View my work
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,36 @@
 import React from 'react';
+// images
+import WomanImg from '../img/home/woman.png';
+// Link
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return <section className='section bg-blue-200'>Home</section>;
+    return (
+        <section className='section'>
+            <div className='container mx-auto h-full relative'>
+                {/* text & img wrapper */}
+                <div className='flex flex-col justify-center'>
+                    <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+                        <h1 className='h1'>
+                            photographer <br />& film maker
+                        </h1>
+                        <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
+                            Los Angeles, USA
+                        </p>
+                        <Link to={'/contact'} className='btn mb-[30px]'>
+                            hire me
+                        </Link>
+                    </div>
+                    {/* image */}
+                    <div className='flex justify-end max-h-96 lg:max-h-max'>
+                        <div className='relative lg:-right-40 overflow-hidden'>
+                            <img src={WomanImg} alt='woman img' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
