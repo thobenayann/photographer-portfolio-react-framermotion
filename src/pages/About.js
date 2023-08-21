71e95562b416d79e4bbccb7397017239b0104f1c
thobenayann <thobena.yann@orange.fr>
2023-08-21 11:08:02 +0200

    transitions on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,10 +3,20 @@ import React from 'react';
 import WomanImg from '../img/about/woman.png';
 // link
 import { Link } from 'react-router-dom';
+// framer motion
+import { motion } from 'framer-motion';
+// transition
+import { transition1 } from '../transitions';
 
 const About = () => {
     return (
-        <section className='section'>
+        <motion.section
+            initial={{ opacity: 0, y: '100%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '100%' }}
+            transition={transition1}
+            className='section'
+        >
             <div className='container mx-auto h-full relative'>
                 {/* text & img wrapper */}
                 <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
@@ -15,7 +25,13 @@ const About = () => {
                         <img src={WomanImg} alt='woman image' />
                     </div>
                     {/* text */}
-                    <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+                    <motion.div
+                        initial={{ opacity: 0, y: '-80%' }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: '-80%' }}
+                        transition={transition1}
+                        className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'
+                    >
                         <h1 className='h1'>About me</h1>
                         <p className='mb-12 max-w-sm'>
                             Sunt amet id exercitation anim. Velit exercitation
@@ -32,10 +48,10 @@ const About = () => {
                         <Link to={'/portfolio'} className='btn'>
                             View my work
                         </Link>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     );
 };
 
