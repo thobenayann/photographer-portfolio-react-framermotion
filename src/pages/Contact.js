255f96b2bc3392bf0d26314abf70627522b52b30
thobenayann <thobena.yann@orange.fr>
2023-08-21 10:41:36 +0200

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,52 @@
 import React from 'react';
+// images
+import WomanImg from '../img/contact/woman.png';
 
 const Contact = () => {
-    return <section className='section bg-amber-600'>Contact</section>;
+    return (
+        <section className='section bg-white'>
+            <div className='container mx-auto h-full'>
+                <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left'>
+                    {/* bg */}
+                    <div className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10' />
+                    {/* text & from */}
+                    <div className='lg:flex-1 lg:pt-32 px-4'>
+                        <h1 className='h1'>Contact me</h1>
+                        <p className='mb-12'>
+                            I would love to get suggestions from you.
+                        </p>
+                        {/* from */}
+                        <form className='flex flex-col gap-y-4'>
+                            <div className='flex gap-x-10'>
+                                <input
+                                    className='outiline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+                                    type='text'
+                                    placeholder='Your name'
+                                />
+                                <input
+                                    className='outiline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+                                    type='text'
+                                    placeholder='Your email address'
+                                />
+                            </div>
+                            <input
+                                className='outiline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+                                type='text'
+                                placeholder='Your message'
+                            />
+                            <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>
+                                Send it
+                            </button>
+                        </form>
+                    </div>
+                    {/* image */}
+                    <div className='lg:flex-1'>
+                        <img src={WomanImg} alt='woman' />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
