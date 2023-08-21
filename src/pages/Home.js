00bd4d9fd4e3bc2bf8218b90d34374902a5288bb
thobenayann <thobena.yann@orange.fr>
2023-08-21 11:03:25 +0200

    transitions on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,30 @@ import React from 'react';
 import WomanImg from '../img/home/woman.png';
 // Link
 import { Link } from 'react-router-dom';
+// framer motion
+import { motion } from 'framer-motion';
+// transition
+import { transition1 } from '../transitions';
 
 const Home = () => {
     return (
-        <section className='section'>
+        <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={transition1}
+            className='section'
+        >
             <div className='container mx-auto h-full relative'>
                 {/* text & img wrapper */}
                 <div className='flex flex-col justify-center'>
-                    <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+                    <motion.div
+                        initial={{ opacity: 0, y: '-50%' }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: '-50%' }}
+                        transition={transition1}
+                        className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'
+                    >
                         <h1 className='h1'>
                             photographer <br />& film maker
                         </h1>
@@ -20,16 +36,27 @@ const Home = () => {
                         <Link to={'/contact'} className='btn mb-[30px]'>
                             hire me
                         </Link>
-                    </div>
+                    </motion.div>
                     {/* image */}
-                    <div className='flex justify-end max-h-96 lg:max-h-max'>
+                    <motion.div
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0 }}
+                        transition={transition1}
+                        className='flex justify-end max-h-96 lg:max-h-max'
+                    >
                         <div className='relative lg:-right-40 overflow-hidden'>
-                            <img src={WomanImg} alt='woman img' />
+                            <motion.img
+                                whileHover={{ scale: 1.1 }}
+                                transition={transition1}
+                                src={WomanImg}
+                                alt='woman img'
+                            />
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     );
 };
 
