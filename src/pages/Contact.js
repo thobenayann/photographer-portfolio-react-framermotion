57fcd60921bc9427a37fd5a408dbe4390c90badf
thobenayann <thobena.yann@orange.fr>
2023-08-21 15:48:18 +0200

    contact transitions

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,30 @@
 import React from 'react';
 // images
 import WomanImg from '../img/contact/woman.png';
+// framer motion
+import { motion } from 'framer-motion';
+// transition
+import { transition1 } from '../transitions';
 
 const Contact = () => {
     return (
-        <section className='section bg-white'>
+        <motion.section
+            initial={{ opacity: 0, y: '100%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '100%' }}
+            transition={transition1}
+            className='section bg-white'
+        >
             <div className='container mx-auto h-full'>
                 <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left'>
                     {/* bg */}
-                    <div className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10' />
+                    <motion.div
+                        initial={{ opacity: 0, y: '100%' }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: '100%' }}
+                        transition={transition1}
+                        className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10'
+                    />
                     {/* text & from */}
                     <div className='lg:flex-1 lg:pt-32 px-4'>
                         <h1 className='h1'>Contact me</h1>
@@ -40,12 +56,18 @@ const Contact = () => {
                         </form>
                     </div>
                     {/* image */}
-                    <div className='lg:flex-1'>
+                    <motion.div
+                        initial={{ opacity: 0, y: '100%' }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: '100%' }}
+                        transition={{ transition1, duration: 1.5 }}
+                        className='lg:flex-1'
+                    >
                         <img src={WomanImg} alt='woman' />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     );
 };
 
