910b358655be492ea361101a82d5c2f0bc0cde1e
thobenayann <thobena.yann@orange.fr>
2023-08-28 10:15:44 +0200

    responsivness on cursor: not showing cursor animation on mobile

--- src/context/CursorContext.js
+++ src/context/CursorContext.js
@@ -12,19 +12,25 @@ const CursorProvider = ({ children }) => {
     // cursor bg state
     const [cursorBG, setCursorBG] = useState('default');
 
-    useEffect(() => {
-        const move = (e) => {
-            setCursorPos({
-                x: e.clientX,
-                y: e.clientY,
-            });
-        };
-        window.addEventListener('mousemove', move);
+    const mobileViewportIsActive = window.innerWidth < 768;
 
-        // remove event
-        return () => {
-            window.removeEventListener('mousemove', move);
-        };
+    useEffect(() => {
+        if (!mobileViewportIsActive) {
+            const move = (e) => {
+                setCursorPos({
+                    x: e.clientX,
+                    y: e.clientY,
+                });
+            };
+            window.addEventListener('mousemove', move);
+
+            // remove event
+            return () => {
+                window.removeEventListener('mousemove', move);
+            };
+        } else {
+            setCursorBG('none');
+        }
     });
 
     // cursor variants
@@ -41,6 +47,11 @@ const CursorProvider = ({ children }) => {
             y: cursorPos.y - 72,
             backgroundColor: '#fff',
             mixBlendMode: 'difference',
+        },
+        none: {
+            width: 0,
+            height: 0,
+            backgroundColor: 'rgba(255,255,255, 1)',
         },
     };
 
